e56dc412254e97a0352f021fc52d387dddb261e5
aomandi068-art <aomondi068@gmail.com>
2026-09-19 15:00:29 +0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const module=document.getElementById("prayer module")
+const form=document.getElementById("prayer form")
+function openmodule(){
+     if (!module)return
+   Module.style.display="flex"
+document.body.classList.add(module-open)
+const firstField=document.getElementById("name")
+if (firstField){
+    setTimeout (() =>firstField.focus(,50){
+    }, Timeout);
+}
+}
